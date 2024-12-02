56e7f602dcf0c0f68cfbbf09675410a114e1f847
Byron Jehoma <Byron.Jehoma@mazars.co.za>
2024-12-02 14:36:14 +0200

    update

--- data.js
+++ data.js
@@ -1,3 +1,131 @@
+var IncidentData = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        ID: 1,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "27/11/2024 00:01:00",
+        Address: "31 Flora Road",
+        Comment:
+          "Owner of property noticed a group of guys passing the house at the time of incident",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.148573, -25.80617],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 2,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "27/11/2024 00:01:00",
+        Address: "29 Flora Rd",
+        Comment: "",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.148365, -25.805847],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 3,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "27/11/2024 00:01:00",
+        Address: "32 Flora Rd",
+        Comment: "",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.149104, -25.805914],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 4,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "25/10/2024 00:00:00",
+        Address: "40 Aero Rd",
+        Comment: "",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.153812, -25.799688],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 5,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "12/09/2024 00:00:00",
+        Address: "16 Gulfoss Rd",
+        Comment: "Two dogs poisoned",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.15626, -25.809782],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 6,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "12/09/2024 00:00:00",
+        Address: "122 Olive Rd",
+        Comment: "Dogs poisoned with chicken",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.155011, -25.802477],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 7,
+        Incident: "Dog Poisoning",
+        Incident_Date_Time: "20/09/2024 00:00:00",
+        Address: "25 Buchan Rd",
+        Comment: "",
+        emoji: "☠️",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.146952, -25.799254],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 8,
+        Incident: "Burglary",
+        Incident_Date_Time: "00/00/2024 00:00:00",
+        Address: "23 Broadway West Rd",
+        Comment: "10h30 this morning and irreplaceable items stolen",
+        emoji: "💥",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.147272, -25.809565],
+      },
+    },
+  ],
+};
+
+/*
 const data = [
   {
     ID: 1,
@@ -36,4 +164,23 @@ const data = [
     Lng: 28.153812,
     Comment: "",
   },
+  {
+    ID: 5,
+    Incident: "Dog Poisoning",
+    Incident_Date_Time: "12/09/2024 00:00:00",
+    Address: "16 Gulfoss Rd",
+    Lat: -25.809782,
+    Lng: 28.15626,
+    Comment: "Two dogs poisoned",
+  },
+  {
+    ID: 6,
+    Incident: "Dog Poisoning",
+    Incident_Date_Time: "12/09/2024 00:00:00",
+    Address: "122 Olive Rd",
+    Lat: -25.802477,
+    Lng: 28.155011,
+    Comment: "Dogs poisoned with chicken",
+  },
 ];
+*/
--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let popup = L.popup();
+let markerColor;
 let map = L.map("map").setView([-25.806391, 28.148789], 16);
+let popup = L.popup();
+let Incident_Data = L.geoJSON(IncidentData, {
+  pointToLayer: function (feature, latlng) {
+    let { Incident } = feature.properties;
+    switch (Incident) {
+      case "Dog Poisoning":
+        markerColor = "#ba0ff9";
+        break;
+      case "Burglary":
+        markerColor = "#fa9346";
+    }
+
+    return L.circleMarker(latlng, {
+      color: markerColor,
+      fillColor: markerColor,
+      fillOpacity: 1,
+      radius: 5,
+    });
+  },
+})
+  .addTo(map)
+  .bindPopup((layer) => {
+    const { Incident, Incident_Date_Time, Address, Comment, emoji } =
+      layer.feature.properties;
+    return `
+    <b>Incident: </b>${emoji} ${Incident}</br>
+    <b>IncidentDate: </b>${Incident_Date_Time}</br>
+    <b>Address: </b>${Address}</br>
+    <b>Comment: </b>${Comment}</br>
+    `;
+  });
+
 let osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-
 let Esri_WorldImagery = L.tileLayer(
   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   {
@@ -21,8 +52,12 @@ let basemaps = {
   "World Imagery": Esri_WorldImagery,
 };
 
+let overlayMaps = {
+  "Incident Data": Incident_Data,
+};
+
 L.Control.geocoder().addTo(map);
-L.control.layers(basemaps).addTo(map);
+L.control.layers(basemaps, overlayMaps).addTo(map);
 
 function onMapClick(e) {
   popup
@@ -33,17 +68,17 @@ function onMapClick(e) {
 map.on("click", onMapClick);
 
 // Show data points on map
-data.forEach(function (item) {
-  console.log(item);
-  L.circle([item.Lat, item.Lng], {
-    color: "red",
-    fillColor: "#f03",
-    fillOpacity: 1,
-    radius: 5,
-  }).addTo(map).bindPopup(`
-      <b>Incident:</b>☠️ ${item.Incident}</br>
-      <b>Reported Date:</b> ${item.Incident_Date_Time}</br>
-      <b>Address:</b> ${item.Address}</br>
-      <b>Comment:</b> ${item.Comment}
-      `);
-});
+// IncidentData.forEach(function (item) {
+//   console.log(item);
+//   L.circle([item.Lat, item.Lng], {
+//     color: "red",
+//     fillColor: "#f03",
+//     fillOpacity: 1,
+//     radius: 5,
+//   }).addTo(map).bindPopup(`
+//       <b>Incident:</b>☠️ ${item.Incident}</br>
+//       <b>Reported Date:</b> ${item.Incident_Date_Time}</br>
+//       <b>Address:</b> ${item.Address}</br>
+//       <b>Comment:</b> ${item.Comment}
+//       `);
+// });
